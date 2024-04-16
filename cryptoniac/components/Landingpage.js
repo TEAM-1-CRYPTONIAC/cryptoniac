932e613ca89ec98191336fa20b5f7c136a4235dc
JormaByte <joel.oksanen1223@gmail.com>
2024-04-16 20:27:06 +0300

    seacrhBar fixed

--- cryptoniac/components/Landingpage.js
+++ cryptoniac/components/Landingpage.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { getCryptoPrices } from './APIKEY';
 import { useTheme } from '../context/ThemeContext';
+import { Icon } from 'react-native-paper'
 
 const LandingPage = ({ navigation }) => {
   const [cryptos, setCryptos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState('');
   const { theme } = useTheme();
 
   useEffect(() => {
@@ -23,6 +25,20 @@ const LandingPage = ({ navigation }) => {
     fetchPrices();
   }, []);
 
+  const handleSearch = text => {
+    const onlyLetters = /^[A-Za-z]+$/
+
+    if (onlyLetters.test(text) || text === '') {
+    setSearchText(text)}
+    else {
+      alert('Please search only with letters of the English alphabet')
+    };
+  };
+
+  const filteredCryptos = cryptos.filter(
+    crypto => crypto.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   if (loading) {
     return (
       <View style={theme.container}>
@@ -33,8 +49,19 @@ const LandingPage = ({ navigation }) => {
 
   return (
     <View style={theme.container}>
+      <View style={theme.searchView}>
+      <Icon color='#4c86f1' source={'database-search'} size={38}/>
+      <TextInput
+        style={theme.searchInput}
+        onChangeText={handleSearch}
+        value={searchText}
+        placeholder="Search by name..."
+        placeholderTextColor={theme.searchInput.placeholderTextColor}
+      />
+      </View>
+      
       <FlatList
-        data={cryptos}
+        data={filteredCryptos}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity
@@ -42,7 +69,7 @@ const LandingPage = ({ navigation }) => {
             onPress={() => navigation.navigate('CryptoDetail', { cryptoId: item.id })}
           >
             <Text style={theme.title}>{item.name} ({item.symbol})</Text>
-            <Text>${item.current_price.toFixed(2)}</Text>
+            <Text style={theme.price}>${item.current_price.toFixed(2)}</Text>
           </TouchableOpacity>
         )}
       />
